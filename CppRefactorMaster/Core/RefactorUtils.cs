2c63fbaf324f6d93874dd5cba0f43301b737d8b8
DeadShot <vladkolodka@gmail.com>
2016-09-07 12:10:49 +0300

    Initial commit

---
+++ CppRefactorMaster/Core/RefactorUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CppRefactorMaster.Core {
+    static class RefactorUtils {
+        static string deleteParams(string source, params string[] paramNames) {
+            return null;
+        }
+
+        static string renameMethod(string source, string oldMmethodName, string newMethodName) {
+            return null;
+        }
+    }
+}
